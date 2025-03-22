a4db4b31bcf990893382cf5df94807f891d40a07
Hetrox Hetrox <kuba.kosiorek2004@gmail.com>
2025-03-22 15:40:13 +0100

    .

---
+++ src/main/java/net/hetrox/crimsonmod/CrimsonMod.java
@@ -0,0 +1,18 @@
+package net.hetrox.crimsonmod;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class CrimsonMod implements ModInitializer {
+
+	public static final String MOD_ID = "crimson-mod";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+
+	}
+}
---
+++ src/main/java/net/hetrox/crimsonmod/CrimsonModClient.java
@@ -0,0 +1,11 @@
+package net.hetrox.crimsonmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CrimsonModClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
